3a4c81df891c4d448cf75e37e94d78588a3ca9e6
Shaun Thompson <neko68k@gmail.com>
2014-01-27 09:38:27 -0700

    first

---
+++ FusionTool/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FusionTool
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream WAC = null;
+            OpenFileDialog ofd1 = new OpenFileDialog();
+
+            ofd1.Filter = "WAC Files (.WAC)|*.WAC";
+            ofd1.FilterIndex = 1;
+
+            ofd1.Multiselect = false;
+
+
+            if (ofd1.ShowDialog() == DialogResult.OK)
+            {
+                WAC = ofd1.OpenFile();
+                initTreeView(WAC);
+            }
+        }
+
+        private void initTreeView(Stream stream)
+        {
+            WAC wac = new WAC(stream);
+            // read root entry from WAC and use its folderCount as a limiter 
+            
+            treeView1.BeginUpdate();
+        }
+    }
+}
---
+++ FusionTool/WAC.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FusionTool
+{
+    class WAC
+    {
+        private struct FOLDER
+        {
+            UInt32 numFiles;
+            UInt32 numFolders;
+            UInt32 filenameOfs;
+            UInt32 fileTOCOfs;
+            UInt32 folderTOCOfs;
+
+        }
+        private struct FILE
+        {
+            UInt32 filenameRelOfs;
+            UInt32 size;					// actual size of file in bytes
+            UInt32 sizeInBlocks;			// loading optimization. multiply by 2048 (DVD block size)
+            UInt32 offset;
+        }
+
+        private Stream inStream;
+
+        public WAC(Stream stream)
+        {
+            inStream = stream;
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public FOLDER GetFolder(String name);
+        public FOLDER[] GetFolders(FOLDER folder);  // get an array of folder children from the passed folder or from root if folder == null
+
+        public FILE GetFile(String name);        
+        public FILE[] GetFiles(FOLDER folder);      // get an array of file children from the passed folder
+
+        public void ReplaceFile(String name, byte[] data);
+
+    }
+}
